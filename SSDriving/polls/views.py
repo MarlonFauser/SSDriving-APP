2987baf46706455c13a5cc56a6c1afb3d9c70e44
Marlon Fauser <marlonfauser>
2018-11-12 20:57:15 -0200

    Polls and Some Views

--- SSDriving/polls/views.py
+++ SSDriving/polls/views.py
@@ -7,3 +7,13 @@ def index(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % 1)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % 1)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % 1)
